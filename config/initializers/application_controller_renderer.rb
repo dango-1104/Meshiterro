139857952080b9271eb267822166b248b427ccf9
dango-1104 <570401mn@gmail.com>
2021-05-16 13:39:11 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '4af5ad726b9d20c14c7b8dca4a43fd71a6555b2601fe4d262eda855db39b812aa019802c3635b257814488795af20bdd1d9ac15e631772322190fbf6b04aa459'
